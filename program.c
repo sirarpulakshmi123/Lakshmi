31509a07ff92d0dc4a69108c48d28650a9943be4
sirarpulakshmi123 <53072927+sirarpulakshmi123@users.noreply.github.com>
2019-08-09 13:13:37 +0530

    Add files via upload

---
+++ program.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#include<conio.h>
+int num;
+flaot total;
+float price;
+int again;
+int c ;
+int main()
+{
+	printf("------- welcome coffe shop-----");
+	printf(" selcet the coffe type");
+	scanf("%d",&c);
+
+	for(0<x<10000)
+     {
+	1<=p<=100;
+     }
+	}
+	void coffee()
+	{
+	int quantity();
+	int again=0;
+	}
+   if(coffee=1)
+   {
+ 	printf("enter quantity :");
+ 	scanf("%d",&quantity);
+ 	total=100*qunatity;
+ 	printf("pay total amount is",total);
+   }
+ 	printf("\n would like to by coffe again?\n[1]yes,[2]no:");
+ 	scanf("%d",&gain);
+ 	system("cls");
+ 	if(again==1)
+ 	{
+ 	printf("enter quantity :");
+ 	scanf("%d",&quantity);
+ 	total=10*qunatity;
+ 	printf("pay total amount is",total);
+ 	}
+ 	if(again==1)
+ 	{
+ 	printf("enter quantity :");
+ 	scanf("%d",&quantity);
+ 	total=1*qunatity;
+ 	printf("pay total amount is",total);
+ 	}
+ 	if (again==1)
+ 	{
+	 printf("enter quantity :");
+ 	scanf("%d",&quantity);
+ 	printf("the coffe is free of cost");
+ 	}
+	
+void exit()
+{
+	printf("thank you very much  \n");
+	printf("come again &&\n\n");
+}
+}
